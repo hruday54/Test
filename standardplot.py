6cd09b18443406dd4e2f109758956a5c45eada80
hruday5 <hruday54@gmail.com>
2021-05-28 02:34:56 +0530

    set

--- standardplot.py
+++ standardplot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# comment
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))  # Plot the sine of each x point
 plt.show()  # Display the plotpython3 -m pip install matplotlibcdcpwd
